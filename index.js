ffeb998092840b7907cc33a5f5f5eb46985f1ace
Junjun1-code <aguilarherrera.nicolas@gmail.com>
2026-04-20 23:28:15 -0400

    Entrega de evaluacion

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+// Lectura de datos con filtrado
+Datos = (JSON.parse(fs.readFileSync("trabajadores.json", "utf8")))
+Datos.forEach(dato => {
+    console.log(`${dato.rut} - ${dato.Nombre}`)    
+})
+
+// Uso de EXPRESS y NODE
+app.listen(3000, console.log('Servidor iniciado'))
+app.use(express.json())
+
+app.get("/trabajadores", (req, res) => {
+    datos = JSON.parse(fs.readFileSync('trabajadores.json'))
+    res.json(datos)
+    res.send('Mostrando datos de trabajadores')
+})
+
+app.post("/trabajadores", (req, res) =>{
+    dato = req.body
+    datos = JSON.parse(fs.readFileSync("trabajadores.json"))
+    datos.push(dato)
+    fs.writeFileSync("trabajadores.json", JSON.stringify(datos))
+    res.send('Se ha agregado un nuevo trabajador al registro')
+})
+
+app.put("/trabajadores/:rut", (req, res) =>{
+    const { rut } = req.params
+    dato = req.body
+    datos = JSON.parse(fs.readFileSync('trabajadores.json'))
+    index = datos.findIndex(t => t.rut == rut)
+    datos[index] = dato
+    fs.writeFileSync('trabajadores.json', JSON.stringify(datos))
+    res.send(`Se ha actualizado el trabajador con el rut ${rut}`)
+})
+
+app.delete("/trabajadores/:rut", (req, res) =>{
+    const {rut} = req.params
+    datos = JSON.parse(fs.readFileSync('trabajadores.json'))
+    index = datos.findIndex(t => t.rut == rut)
+    datos.splice(index, 1)
+    fs.writeFileSync('trabajadores.json', JSON.stringify(datos))
+    res.send('Se elimino un trabajador del registro')
+})
